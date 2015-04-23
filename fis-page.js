afdca8288228ada2cc65408eeb30fcf6a245ca2b
煎饼 <crossyou2009@gmail.com>
2015-04-23 10:17:45 +0800

    0.0.1

---
+++ fis-page.js
@@ -0,0 +1,12 @@
+/*
+ * fis-page
+ *
+ * Licensed under the MIT license.
+ * https://github.com/exp-team/fis-page/blob/master/LICENSE
+ */
+
+'use strict';
+
+module.exports = function(fis) {
+    //TODO
+}
